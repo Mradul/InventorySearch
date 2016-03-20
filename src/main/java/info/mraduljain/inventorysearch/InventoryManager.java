5a5d4af0adc148c818eee8c19ecaeb0d0271c82a
mradul <mradul.exe@gmail.com>
2016-03-20 00:20:54 -0500

    Question 2 done in a separate method

--- src/main/java/info/mraduljain/inventorysearch/InventoryManager.java
+++ src/main/java/info/mraduljain/inventorysearch/InventoryManager.java
@@ -57,18 +57,10 @@ public class InventoryManager {
 		Map<String,List<ProductWithPrice>> productTypes = im.getProductCategoryMap(productInventory);
 		//System.out.println(productTypes);
 		//Which cds have a total running time longer than 60 minutes?
-		List<ProductWithPrice> cdList=productTypes.get("cd");
-		for(ProductWithPrice aCD:cdList){
-			JSONObject prod= (JSONObject) aCD.getProduct();
-			System.out.println(prod.get("tracks"));
-			JSONArray tracks=(JSONArray) prod.get("tracks");
-			int totalSeconds=0;
-			for(Object trk:tracks){
-				JSONObject aTrack= (JSONObject) trk;
-				totalSeconds+=Integer.parseInt(""+aTrack.get("seconds"));
-			}
-			System.out.println(totalSeconds);
-		}
+		int thresholdMinutes=60;
+		int SEC_PER_MIN=60;
+		System.out.println("CDs that have total running time longer than "+thresholdMinutes+" minutes \n"
+							+im.getCDsWithGreaterRunTime(productTypes,thresholdMinutes*SEC_PER_MIN));
 		
 		Iterator it = productTypes.entrySet().iterator();
 	    while (it.hasNext()) {
@@ -134,5 +126,25 @@ public class InventoryManager {
 			
 		}
 		return productTypes;
+	}
+	
+	public List<JSONObject> getCDsWithGreaterRunTime(Map<String,List<ProductWithPrice>> productTypes,int thresholdTimeInSecs){
+		List<ProductWithPrice> cdList=productTypes.get("cd");
+		List<JSONObject> cdListWithThresholdRuntime = new ArrayList<JSONObject>();
+		for(ProductWithPrice aCD:cdList){
+			JSONObject prod= (JSONObject) aCD.getProduct();
+			//System.out.println(prod.get("tracks"));
+			JSONArray tracks=(JSONArray) prod.get("tracks");
+			int totalSeconds=0;
+			for(Object trk:tracks){
+				JSONObject aTrack= (JSONObject) trk;
+				totalSeconds+=Integer.parseInt(""+aTrack.get("seconds"));
+			}
+			System.out.println(totalSeconds);
+			if(totalSeconds>thresholdTimeInSecs){
+				cdListWithThresholdRuntime.add(prod);
+			}
+		}
+		return cdListWithThresholdRuntime;
 	}
 }
